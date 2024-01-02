0375ffa5ef544b8f195de7dcc82e44dddcbaf457
advancedvirtual2 <148706555+advancedvirtual2@users.noreply.github.com>
2024-01-03 01:33:03 +0400

    Update extcmd_txfetch.py

--- files/extcmd_txfetch.py
+++ files/extcmd_txfetch.py
@@ -1,4 +1,3 @@
-#import art#
 from art import *
 from colorama import Style as S
 from colorama import Fore  as F
@@ -16,20 +15,21 @@ class extcmd_handler_main():
         self.td = ____td____
     def txfetch(self, cwd:str=".", args:list=[]):
         system = self.td.sname
-        version = self.td.sversion ##}#ae #[0]# #
+        host_system = platform.system() ##y##
+        version = self.td.sversion
         release = platform.release()
         #len#False
         # Display ASCII art logo
         logo = text2art(system)
-        if "--nocolor" in args or "-n" in args: #args.get(0)=="--nocolor" or args.get(0)=="-nc":#
+        if "--nocolor" in args or "-n" in args:
             print(logo)
         else:    
             print(S.BRIGHT+F.CYAN+logo+S.RESET_ALL)
 
         # Display system information
         print(f"System: {system}")
-        print(f"Release: {release}")
         print(f"Version: {version}")
+        print(f"Host System: {host_system} {release}")
 
         if "--help" in args or "-h" in args: #iiher
             print(f"\n\n---------------\nHelp for \"{self.safe_list_get(self.commands, 0, {}).get('name', 'txfetch')}\":\n--help / -h : show this help message.\n--nocolor / -n : disable colors.\n---------------") 
