f25a8564535091e9bccd8d5066ed1bae017fd8c5
advancedvirtual2 <148706555+advancedvirtual2@users.noreply.github.com>
2024-01-03 02:15:31 +0400

    Create extcmd_cat.py

---
+++ files/extcmd_cat.py
@@ -0,0 +1,23 @@
+
+
+class extcmd_handler_main():
+    td = None    
+           
+    def __init__(self, ____td____):
+        self.td = ____td____
+    def cat(cwd:str=".", args:list=[]):
+      for i in args:
+        path=None
+        if not i.startswith("/"):#os.path.join(".",i) self, #+i
+          path=os.path.join(cwd,i)
+        else:
+          path=os.path.join(".",i)
+        with open(path, 'r') as fr:
+          print(fr.read())
+      print("\n") ###".")    
+        #if not i.startswith("/"):
+        #  path=cwd+args
+      
+    commands = [
+        {"name": "cat", 'function': cat, 'neededArgs': True, 'description': 'Like a Linux "cat" command, print all contents of all files in arguments..."} ###) ###commandsfor\"linux
+         ]
