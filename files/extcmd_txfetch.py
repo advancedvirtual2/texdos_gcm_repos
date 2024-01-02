c2a523ec63eabe731e26d443a54c70c744160c17
advancedvirtual2 <148706555+advancedvirtual2@users.noreply.github.com>
2024-01-03 01:04:25 +0400

    Update extcmd_txfetch.py

--- files/extcmd_txfetch.py
+++ files/extcmd_txfetch.py
@@ -5,6 +5,11 @@ from colorama import Fore  as F
 import platform
 
 class extcmd_handler_main():
+    def safe_list_get (l, idx, default):
+      try:
+        return l[idx]
+      except IndexError:
+        return default
     td = None    
            
     def __init__(self, ____td____):
@@ -27,7 +32,7 @@ class extcmd_handler_main():
         print(f"Version: {version}")
 
         if "--help" in args or "-h" in args: #iiher
-            print(f"\n\n---------------\nHelp for \"{self.commands.get(0, {}).get('name', 'txfetch')}\":\n--help / -h : show this help message.\n--nocolor / -n : disable colors.\n---------------") 
+            print(f"\n\n---------------\nHelp for \"{self.safe_list_get(self.commands, 0, {}).get('name', 'txfetch')}\":\n--help / -h : show this help message.\n--nocolor / -n : disable colors.\n---------------") 
       
     commands = [
         {"name": "txfetch", 'function': txfetch, 'neededArgs': True, 'description': 'Like a neofetch program.' , 'neededSelf': True}
