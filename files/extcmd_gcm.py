5c8ce38a9c594a01363e71a9f05c2443d7dea56e
advancedvirtual2 <148706555+advancedvirtual2@users.noreply.github.com>
2024-01-03 01:40:22 +0400

    Update extcmd_gcm.py
    Removed incorrect comments.
    .

--- files/extcmd_gcm.py
+++ files/extcmd_gcm.py
@@ -12,18 +12,16 @@ class extcmd_handler_main():
            
     def __init__(self, ____td____):
         self.td = ____td____
-        ###self.gcm_dir=os.path.join(self.td.uname, ".gcm")## #
-        ###self.cache_directory=os.path.join(self.gcm_dir, "cache")## #
     
-    git_base_url_usercontent = "https://raw.githubusercontent.com/{repos_name}/main/{file}" #lr ## #.strip().sp    ##file_list.txt
+    git_base_url_usercontent = "https://raw.githubusercontent.com/{repos_name}/main/{file}"
     
-    def get_repos_filelist(self, repos_name:str, replace_sc:bool=False): ##D): ###=""##  #
+    def get_repos_filelist(self, repos_name:str, replace_sc:bool=False):
         _repos_name_=repos_name
         if replace_sc: _repos_name_=_repos_name_.replace(';', '/', 1)
         r=requests.get(self.git_base_url_usercontent.format(repos_name=_repos_name_, file="file_list.txt"))
         if r.status_code != 200:
             raise Exception(f"Can not download file list of repository: \"{repos_name}\"")
-        return r.text ###.splitlines()###
+        return r.text 
     
     def list(self, name:str=""):
         if name == "" or name == None:
@@ -33,14 +31,13 @@ class extcmd_handler_main():
                      tqdm.tqdm.write(i)
                      repos_name=i
                      fr=open(os.path.join(self.gcm_dir, "cache", f"{repos_name.replace('/', ';')}.txt"), 'r')
-                     dsi=fr.read() ### ##read. #
+                     dsi=fr.read()
                      fr.close()
-                     ### ## #fdsirads #a##  ###
                      for ii in dsi.splitlines():
                          data=f"{ii.split('|')[0]}=={ii.split('|')[1]}"
                          
                          tqdm.tqdm.write(data)
-        else: ###w                 ##  #
+        else:
             repos_name=name
             fr=open(os.path.join(self.gcm_dir, "cache", f"{repos_name}.txt"), 'r')
             dsi=fr.read()
@@ -93,7 +90,7 @@ class extcmd_handler_main():
         url=self.git_base_url_usercontent.format(repos_name=repos_name, file=f"files/extcmd_{name}.py")
         print(F.YELLOW+f"Downloading file: \"{url}\"..."+S.RESET_ALL)
         r=requests.get(url)
-        if r.status_code != 200: #@==(
+        if r.status_code != 200:
             r.raise_for_status()
         print(F.GREEN+f"Received {len(r.content)} bytes."+S.RESET_ALL)    
         if os.path.exists(os.path.join("system", f"extcmd_{name}.py")): print("Moving old version...") ; shutil.move(os.path.join("system", f"extcmd_{name}.py"), os.path.join("system", f"no_extcmd_{name}_{ttt.shttxt(str(datetime.datetime.now()), '_')}.py")) #verssssss#{}"#isfile    
@@ -101,14 +98,11 @@ class extcmd_handler_main():
         with open(os.path.join("system", f"extcmd_{name}.py"), 'ab') as fab:
             bytecount=fab.write(r.content)
         
-            print(F.GREEN+f"Written {bytecount} bytes."+S.RESET_ALL) ###Writtenwfab.
-        print(F.GREEN+S.BRIGHT+"OK!"+S.RESET_ALL) ###>.S>RES)
-        time.sleep(0.135) ###0)
-        #print(F.YELLOW+"REBOOTING...!#
-        print("Running command: \"reboot\"...") ###'|#(#"running reboot...")
-        self.td.processCommand(command='reboot')  
-        #time.sleep(1)  
-            #$r$#aise #HTTPException #
+            print(F.GREEN+f"Written {bytecount} bytes."+S.RESET_ALL)
+        print(F.GREEN+S.BRIGHT+"OK!"+S.RESET_ALL)
+        time.sleep(0.135)
+        print("Running command: \"reboot\"...")
+        self.td.processCommand(command='reboot') 
         
     def gcm(self, cwd:str=".", args:list=[]):
       name=""
