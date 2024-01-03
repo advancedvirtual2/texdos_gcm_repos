3a6abb352d83ad7dca2182f7c9c795495294afcf
65647m <artemmitygov@yandex.ru>
2024-01-03 22:36:49 +0400

    Fixed...

--- files/extcmd_troot_is_root.py
+++ files/extcmd_troot_is_root.py
@@ -1,23 +1,19 @@
 class extcmd_handler_main:
     td=None
-    def __init__(self, __td__):
-        self.td=__td__
-        def rename_troot(payload_):
+    def rename_troot(self, payload_):
             td=payload_.get('system')
-            #payload=payload_.get('payload')
-            #td.externalCommands #
-            #isFounded=False#
             root=None
             troot=None
             for i in td.externalCommands :
                 if i.get('name') == 'root':
-                    print("\"root\" command is already exists") ##self, # 
-                    return ###extpresent") ###("Root")###
+                    print("\"root\" command is already exists")
+                    return
                 if i.get('name') == 'troot':
                     troot=i
             if troot == None: print("\"troot\" command is not found!"); return 
             else:
                 troot['name']='root'
-            ### //--// --#m
-        self.td.Events.AfterInit.append(rename_troot)
+    def __init__(self, __td__):
+        self.td=__td__
+        self.td.Events.AfterInit.append(self.rename_troot)
     commands=[]
