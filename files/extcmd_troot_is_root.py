ca6e24082f061180b39faf5a77a39c22c33df626
65647m <artemmitygov@yandex.ru>
2024-01-03 22:27:16 +0400

    Added "troot_is_root" extension v0.51b .

---
+++ files/extcmd_troot_is_root.py
@@ -0,0 +1,23 @@
+class extcmd_handler_main:
+    td=None
+    def __init__(self, __td__):
+        self.td=__td__
+        def rename_troot(self, payload_):
+            td=payload_.get('system')
+            #payload=payload_.get('payload')
+            #td.externalCommands #
+            #isFounded=False#
+            root=None
+            troot=None
+            for i in td.externalCommands :
+                if i.get('name') == 'root':
+                    print("\"root\" command is already exists") ### 
+                    return ###extpresent") ###("Root")###
+                if i.get('name') == 'troot':
+                    troot=i
+            if troot == None: print("\"troot\" command is not found!"); return 
+            else:
+                troot['name']='root'
+            ### //--// --#m
+        self.td.Events.AfterInit.append(rename_troot)
+    commands=[]
